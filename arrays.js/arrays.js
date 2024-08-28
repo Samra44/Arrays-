774a5e089817cbd2cd004a2778beab0eb2e3e5ac
Samra44 <154986251+Samra44@users.noreply.github.com>
2024-08-28 16:42:11 +0500

    Add files via upload
    Arrays.js

---
+++ arrays.js/arrays.js
@@ -0,0 +1,33 @@
+let arr = [2,4,6,8,10,12]
+console.log(arr[5])
+console.log(arr[-1])
+arr[2]=100 // updated the array element
+arr[6]=200 // updated the array element
+arr[8]=500 // updated the array element
+arr[7] // out put show empty
+console.log(arr)
+console.log(arr)
+
+// type of 
+console.log (typeof arr)
+// lenght
+console.log (arr.length)//.notataion method
+
+console.log (arr.toString()) 
+
+let student_data = ["Ali","Umar","Taha"]
+// pop delete the last element
+console.log(student_data.pop())
+console.log(student_data)
+// push method add the elemnt at the last 
+console.log (student_data.push("Dua","Ramesha"))
+console.log(student_data)
+console.log(student_data.sort()) // sort method
+console.log(student_data.includes("Ramsha"))// sort method
+
+// pop method
+Letx = ["cat","Dog","Loin","Tiger"]
+console.log(x.shift())
+// console.log(x)
+// console.log(x.unshift("camel","cow"))
+// coleons.log(x)
